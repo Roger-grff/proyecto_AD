541473794a01f72a551296ed18467aa1aa61f4a4
Roger Grefa <kaell.room@gmail.com>
2026-07-21 16:16:55 -0500

    limitacion de consumo

--- proyecto_AD/tests/test.js
+++ proyecto_AD/tests/test.js
@@ -2,13 +2,29 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-    vus: 500,
-    duration: '3m',
+    scenarios: {
+        usuarios_100: {
+            executor: 'constant-vus',
+            vus: 100,
+            duration: '3m',
+        },
+        usuarios_300: {
+            executor: 'constant-vus',
+            vus: 300,
+            duration: '3m',
+            startTime: '3m', // Empieza cuando termina el primero
+        },
+        usuarios_500: {
+            executor: 'constant-vus',
+            vus: 500,
+            duration: '3m',
+            startTime: '6m', // Empieza cuando termina el segundo
+        },
+    },
 };
 
 export default function () {
-
-    let res = http.get('http://host.docker.internal:8080/');
+    const res = http.get('http://host.docker.internal:8080/');
 
     check(res, {
         'status es 200': (r) => r.status === 200,
@@ -16,6 +32,5 @@ export default function () {
 
     sleep(1);
 }
-
 //docker run --rm -i -v "${PWD}:/scripts" grafana/k6 run /scripts/test.js
 //docker stats
